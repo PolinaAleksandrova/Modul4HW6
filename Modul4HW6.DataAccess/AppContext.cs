e59d211026b285b10cb3c123e8038995e0bee7bb
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2021-11-22 05:24:30 +0200

    query1

--- Modul4HW6.DataAccess/AppContext.cs
+++ Modul4HW6.DataAccess/AppContext.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Modul4HW6.DataAccess.Configurations;
+using Modul4HW6.DataAccess.Models;
+
+namespace Modul4HW6.DataAccess
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<ArtistSong> ArtistSongs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Song> Songs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ArtistConfiguration());
+            modelBuilder.ApplyConfiguration(new GenreConfiguration());
+            modelBuilder.ApplyConfiguration(new SongConfiguration());
+            modelBuilder.ApplyConfiguration(new ArtistSongConfiguration());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.LogTo(Console.Write);
+        }
+    }
+}
